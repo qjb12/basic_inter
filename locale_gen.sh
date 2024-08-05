fa26b9b115d071f306a42e7d835def45642c451a
Quinn Broderick <qjbroderick@ucdavis.edu>
2024-08-05 14:04:03 -0700

    added package to locale_gen.sh

--- locale_gen.sh
+++ locale_gen.sh
@@ -2,6 +2,7 @@
 
 apk add --no-cache libc-utils
 apk add --no-cache zip
+apk add --no-cache glibc-i18n
 localedef -i es_ES -f UTF-8 es_ES.UTF-8
 localedef -i ja_JP -f UTF-8 ja_JP.UTF-8
 localedef -i en_US -f UTF-8 en_US.UTF-8
